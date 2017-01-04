0cac85fcb79463356ae79445658e9c20556514e2
JackBrasesco <jacbras@nuevaschool.org>
2017-01-04 10:04:02 -0800

    Classadoo autocommit

--- stuff.js
+++ stuff.js
@@ -1,6 +1,9 @@
 $("#blah").keydown(function(e) {
   if (e.keyCode == 13) {
     var problem = $("blah").val()
-    var step1 = problem.split("*")
+    var step1 = problem.split("*");
+    var firstNumber = step1[0]
+    var secondNumber = step1[1]
+    var step2 = 
   }
 })
